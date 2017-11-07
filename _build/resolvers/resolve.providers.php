18adb243a4db737cb5e3acc559aebdfdcbbdb4a6
Burick <cyberburick@gmail.com>
2017-11-07 19:06:01 +0200

    Добавлены провайдеры

--- _build/resolvers/resolve.providers.php
+++ _build/resolvers/resolve.providers.php
@@ -19,6 +19,46 @@ if ($object->xpdo) {
 				));
 				$provider->save();
 			}
+/* Добавлено */
+		    $provider_name = 'rest.modxstore.ru';
+			if (!$provider = $modx->getObject('transport.modTransportProvider', array('service_url:LIKE' => '%' . $provider_name . '%'))) {
+				$provider = $modx->newObject('transport.modTransportProvider', array(
+					'name' => $provider_name,
+					'service_url' => 'http://' . $provider_name . '/extras/',
+					'username' => !empty($options['email']) && preg_match('/.+@.+\..+/i', $options['email']) ? trim($options['email']) : '',
+					'api_key' => !empty($options['key']) ? trim($options['key']) : '',
+					'description' => 'Репозиторий Николая Ланца ' . $provider_name,
+					'created' => time(),
+				));
+				$provider->save();
+			}
+
+		    $provider_name = 'extras.melting-media.com';
+			if (!$provider = $modx->getObject('transport.modTransportProvider', array('service_url:LIKE' => '%' . $provider_name . '%'))) {
+				$provider = $modx->newObject('transport.modTransportProvider', array(
+					'name' => $provider_name,
+					'service_url' => 'http://extras'.$provider_name,
+					'username' => !empty($options['email']) && preg_match('/.+@.+\..+/i', $options['email']) ? trim($options['email']) : '',
+					'api_key' => !empty($options['key']) ? trim($options['key']) : '',
+					'description' => 'Бесплатный репозиторий с компонентами, разработанными в Melting Media ' . $provider_name,
+					'created' => time(),
+				));
+				$provider->save();
+			}
+
+		    $provider_name = 'rstore.pro';
+			if (!$provider = $modx->getObject('transport.modTransportProvider', array('service_url:LIKE' => '%' . $provider_name . '%'))) {
+				$provider = $modx->newObject('transport.modTransportProvider', array(
+					'name' => $provider_name,
+					'service_url' => 'http://' . $provider_name . '/extras/',
+					'username' => !empty($options['email']) && preg_match('/.+@.+\..+/i', $options['email']) ? trim($options['email']) : '',
+					'api_key' => !empty($options['key']) ? trim($options['key']) : '',
+					'description' => 'Репозиторий Владимира Гришина ' . $provider_name,
+					'created' => time(),
+				));
+				$provider->save();
+			}
+
 			break;
 		case xPDOTransport::ACTION_UNINSTALL:
 			break;
