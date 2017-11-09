9e16fa8e994d9173128a13fae55782946999c025
Burick <cyberburick@gmail.com>
2017-11-09 16:19:23 +0200

    Изменен ресолвер пример

--- _build/resolvers/resolve.sample.php
+++ _build/resolvers/resolve.sample.php
@@ -1,19 +1,22 @@
 <?php
 
-if ($object->xpdo) {
-	/** @var modX $modx */
-	$modx =& $object->xpdo;
-    
-    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
-		case xPDOTransport::ACTION_INSTALL:
-		    $modx->log(modX::LOG_LEVEL_INFO, 'Run <b>Sample</b> resolver install');
-			break;
-		case xPDOTransport::ACTION_UPGRADE:
-			$modx->log(modX::LOG_LEVEL_INFO, 'Run <b>Sample</b> resolver upgrade');
-			break;
+/** @var $modx modX */
+if (!$modx = $object->xpdo AND !$object->xpdo instanceof modX) {
+    return true;
+}
 
-		case xPDOTransport::ACTION_UNINSTALL:
-			break;
-	}
+switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+    case xPDOTransport::ACTION_INSTALL:
+        $modx->log(modX::LOG_LEVEL_INFO, 'Run <b>Sample</b> resolver install');
+        break;
+    case xPDOTransport::ACTION_UPGRADE:
+        $modx->log(modX::LOG_LEVEL_INFO, 'Run <b>Sample</b> resolver upgrade');
+        break;
+
+    case xPDOTransport::ACTION_UNINSTALL:
+        break;
 }
 return true;
+
+
+
